547c39724b85c45ac0814a232cba71f6caed6653
LAmb-DEX <ambrosio.laura@dreamexchange.onmicrosoft.com>
2024-12-16 16:43:23 +0900

    Update install_dependencies.sh
    changing php commands

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,12 +3,11 @@
 # Updates and installs apache and PHP
 sudo yum update -y
 sudo yum install -y httpd
-sudo yum install -y php
+sudo amazon-linux-extras install -y php8.2
 
 # Starts and enable the services
 sudo systemctl start httpd
-sudo systemctl start php
+sudo systemctl start php-fpm
 
 sudo systemctl enable httpd
-sudo systemctl enable php
-
+sudo systemctl enable php-fpm
